f398a191c8455b8f67179836ae8a15ce5e98f36d
Austin Casey <thepoast@gmail.com>
2019-09-30 19:48:49 -0500

    bweee

--- ClinkedIn-Blue-Rangers/DataAccess/ClinkerRepository.cs
+++ ClinkedIn-Blue-Rangers/DataAccess/ClinkerRepository.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClinkedIn_Blue_Rangers.Models;
 
 namespace ClinkedIn_Blue_Rangers.DataAccess
 {
     public class ClinkerRepository
     {
+        static List<Clinker> _clinkers = new List<Clinker>
+        {
+            new Clinker
+            {
+
+            },
+            new Clinker
+            {
+
+            },
+            new Clinker
+            {
+
+            },
+        };
     }
 }
