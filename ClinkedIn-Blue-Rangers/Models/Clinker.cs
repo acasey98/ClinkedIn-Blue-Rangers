2854fcbcc5ae11ef784d54daca9f634015950769
David Mullins <Mullins6323@gmail.com>
2019-09-30 19:12:58 -0500

    file structure

---
+++ ClinkedIn-Blue-Rangers/Models/Clinker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn_Blue_Rangers.Models
+{
+    public class Clinker
+    {
+    }
+}
